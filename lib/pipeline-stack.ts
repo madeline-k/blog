59b3e2e4a3eeb28a481f7172940c2b7f0696f5ea
Madeline Kusters <mkusters@amazon.com>
2022-04-10 19:46:14 -0700

    add stage

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { WebsiteStage } from './website-stage';
 
 const GITHUB_CONNECTION_ARN = 'arn:aws:codestar-connections:us-west-2:594509634114:connection/023024eb-c283-41a0-9297-054dbd2efec0';
 
@@ -21,6 +22,10 @@ export class PipelineStack extends Stack {
         ],
       }),
     });
+
+    pipeline.addStage(new WebsiteStage(this, 'Website', {
+      env: { account: '594509634114', region: 'us-west-2' },
+    }))
   }
 }
 
